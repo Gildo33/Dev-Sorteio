21ba4695a8ff1bd971210a512cc8dcf7e092ee32
Gildo33 <gildofelismino38@gmailcom>
2024-08-21 21:41:36 -0300

    2° projeto js

---
+++ 2.2DevSorteio.js
@@ -0,0 +1,24 @@
+/*const devsorteio = "Estou aqui"
+console.log(devsorteio)*/
+
+
+
+function botão () {
+    const min = Math.ceil(document.querySelector(".input").value)
+    const max = Math.floor(document.querySelector(".input2").value)
+    //const result = document.querySelector(".input3").value
+    
+
+    const resultado1 = Math.floor(Math.random() * (max - min + 1))+min;
+
+      
+     alert (resultado1)
+    
+   
+
+}
+
+
+result.addEventListener("change",botão ).value
+
+
